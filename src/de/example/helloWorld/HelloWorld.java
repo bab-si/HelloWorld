84b6fa2d72f3690d57cfe8dfac1a554c4e75c342
Barbara Hoffmann <barbara.hoffmann@uni-bayreuth.de>
2024-09-10 15:21:37 +0200

    augment HelloWorld example

--- src/de/example/helloWorld/HelloWorld.java
+++ src/de/example/helloWorld/HelloWorld.java
@@ -3,7 +3,12 @@ package de.example.helloWorld;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        // Ausgabe Hello World!
-        System.out.println("Hello World!");
+        System.out.println("Hello \n World");
+        System.out.println("C:\\programmierung\\java");
+
+        char singleQuoteFirst = '\'';
+        System.out.println("Das ist ein einfaches Anführungszeichen als Zeichen: " + singleQuoteFirst);
+
+        System.out.println("Sie sagte: \"Java ist super\"");
     }
 }
