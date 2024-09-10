7ae57f342c261f492972fafa0b43fd801a9b1d1f
Barbara Hoffmann <barbara.hoffmann@uni-bayreuth.de>
2024-09-10 16:48:08 +0200

    add DatatypesAndVariables example

---
+++ src/de/example/helloWorld/DatatypesAndVariables.java
@@ -0,0 +1,13 @@
+package de.example.helloWorld;
+
+public class DatatypesAndVariables {
+
+    public static void main(String[] args) {
+
+        int a;
+        int b = 5;
+        System.out.println(b);
+        char x = 'm';
+        System.out.println(x);
+    }
+}
